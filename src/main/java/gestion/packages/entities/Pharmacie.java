13f3dbb08eb0a972de23001b21dd914b958b1732
zineb <126409044+catmilkomg@users.noreply.github.com>
2023-06-03 17:19:45 +0100

    auth

--- src/main/java/gestion/packages/entities/Pharmacie.java
+++ src/main/java/gestion/packages/entities/Pharmacie.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -28,6 +29,7 @@ public class Pharmacie {
 	private String adress;
 	private Double latitude;
 	private Double longitude;
+	@Column(length = 65535)
 	private String image;
 
 	@ManyToOne
